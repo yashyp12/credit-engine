52d17cdaee98690920bb0a6d14f2ac75c298cf37
Yash Patil <patilyashyp12@gmail.com>
2025-06-16 16:56:35 +0530

    added use post method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ app.use(express.json());
 
 app.use('/api', apiRoutes);
 
+app.get('/api/enroll', (req, res) => {
+  res.send(' Use POST method to enroll a user. This endpoint does not support GET requests.');
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
